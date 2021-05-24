9cd00812196147ec87a08f4c46ded3c7f97a34da
husisusi <git@subhis.de>
2021-05-24 21:04:48 +0200

    Add input argument and log age
    check_rundeck.sh -H DB_HOST_AP -u DB_USER -p DB_Passwor -d DB_Name"
    (is optional default=48) -a Not_Older_Than_Hours

--- check_rundeck_jobs.sh
+++ check_rundeck_jobs.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-############################
-DBUSER=""            # DB USERNAME
-DBPASS=""            # DB PASSWORD
-DBHOST=""            # RUNDECK DB Server HOSTNAME
-DBNAME=""            # DB Name
-############################
+set -e
+AGE=48
 
-data=$(echo "select rundeck.base_report.report_id,rundeck.base_report.date_started,rundeck.base_report.status from rundeck.base_report WHERE rundeck.base_report.status NOT LIKE 'succeed'"\
+while getopts H:u:p:d:a: flag
+do
+    case "${flag}" in
+        H) DBHOST=${OPTARG};;
+        u) DBUSER=${OPTARG};;
+        p) DBPASS=${OPTARG};;
+        d) DBNAME=${OPTARG};;
+        a) AGE=${OPTARG};;
+    esac
+done
+
+if [ $# -lt 8 ]
+  then
+    echo "No enough arguments supplied!!!"
+    echo "To use it do: check_rundeck.sh -H DB_HOST_AP -u DB_USER -p DB_Passwor -d DB_Name"
+    echo "(optional default=48) -a Not_Older_Than_Hours"
+exit
+fi
+
+data=$(echo "select rundeck.base_report.report_id,rundeck.base_report.date_started,rundeck.base_report.status from rundeck.base_report WHERE rundeck.base_report.status NOT LIKE 'succeed' AND rundeck.base_report.date_started > DATE_SUB(NOW(), INTERVAL ${AGE} HOUR)"\
 | mysql -D${DBNAME} -u${DBUSER} -h${DBHOST} -p${DBPASS} -N)
 
 data1=$(echo $data | awk 'NF>1{print $NF}')
-
 
 if [ "${data1}" = '' ]; then
                 echo "OK - All succeed "
